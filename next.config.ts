b159b730124b763a7b51fcbd8ee68b643ecb5567
Mars Wave <fromhearttomars@gmail.com>
2025-01-26 21:10:40 +0100

    Build fixes

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: "export",
-  // basePath: '/marswave',
-  // assetPrefix: '/marswave/',
-  // trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
