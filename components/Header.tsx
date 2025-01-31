1479ca5cd84d242f369a4b6f416a26fdde1249b0
Mars Wave <fromhearttomars@gmail.com>
2025-01-31 18:52:12 +0100

    Adjust layout

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,8 +2,11 @@
 import React from 'react';
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils"
 
 const Header = () => {
+  const pathname = usePathname();
 
   const menuItems = [
     { name: 'ABOUT', link: '/about' },
@@ -13,7 +16,7 @@ const Header = () => {
   return <div className="fixed z-30 flex gap-8 items-center w-full h-14 text-white animate-fadeIn opacity-0 transition-opacity m-4">
     <Link href="/"><Image className="opacity-70" src="/marswave_logo_white.svg" alt="Marswave Logo" width="100" height="43" /></Link>
     {menuItems.map((item) =>
-      <Link href={item.link} key={item.name} className='text-white opacity-70 hover:opacity-100 transition-opacity'>
+      <Link href={item.link} key={item.name} className={cn("opacity-70 hover:opacity-100 transition-opacity", `${pathname === item.link ? "text-destructive active opacity-100" : "text-white "}`)}>
         {item.name}
       </Link>
     )}
